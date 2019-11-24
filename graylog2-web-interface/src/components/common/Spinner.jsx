587c8b5358d7f8818ae2ce2f1436f73725623c0a
Linus Pahl <lpahl@protonmail.com>
2019-11-24 22:39:31 +0100

    Use hooks for Spinner instead of a class component

--- graylog2-web-interface/src/components/common/Spinner.jsx
+++ graylog2-web-interface/src/components/common/Spinner.jsx
@@ -1,11 +1,12 @@
 // @flow
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
+import type { ComponentType } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import Icon from './Icon';
 
-const Wrapper: React.ComponentType<{ visible: boolean }> = styled.span`
+const Wrapper: ComponentType<{ visible: boolean }> = styled.span`
   visibility: ${({ visible }) => (visible ? 'visible' : 'hidden')};
 `;
 
@@ -15,59 +16,39 @@ type Props = {
   text?: string,
 }
 
-type State = {
-  delayFinished: boolean
-}
-
 /**
  * Simple spinner to use while waiting for something to load.
  */
-class Spinner extends React.Component<Props, State> {
-  delayTimeout = undefined
+const Spinner = ({ name, text, delay, ...rest }: Props) => {
+  const [delayFinished, setDelayFinished] = useState(false);
 
-  static propTypes = {
-    /** Delay in ms before displaying the spinner */
-    delay: PropTypes.number,
-    /** Name of the Icon to use. */
-    name: PropTypes.string,
-    /** Text to show while loading. */
-    text: PropTypes.string,
-  };
-
-  static defaultProps = {
-    name: 'spinner',
-    text: 'Loading...',
-    delay: 200,
-  };
-
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      delayFinished: false,
-    };
-  }
-
-  componentDidMount() {
-    const { delay } = this.props;
-    this.delayTimeout = window.setTimeout(() => {
-      this.setState({
-        delayFinished: true,
-      });
+  useEffect(() => {
+    const delayTimeout = window.setTimeout(() => {
+      setDelayFinished(true);
     }, delay);
-  }
 
-  componentWillUnmount() {
-    if (this.delayTimeout) {
-      clearTimeout(this.delayTimeout);
-    }
-  }
+    return () => {
+      clearTimeout(delayTimeout);
+    };
+  }, []);
 
-  render() {
-    const { name, text, ...rest } = this.props;
-    const { delayFinished } = this.state;
+  return <Wrapper visible={delayFinished}><Icon name={name} spin {...rest} /> {text}</Wrapper>;
+};
 
-    return <Wrapper visible={delayFinished}><Icon name={name} spin {...rest} /> {text}</Wrapper>;
-  }
-}
+
+Spinner.propTypes = {
+  /** Delay in ms before displaying the spinner */
+  delay: PropTypes.number,
+  /** Name of the Icon to use. */
+  name: PropTypes.string,
+  /** Text to show while loading. */
+  text: PropTypes.string,
+};
+
+Spinner.defaultProps = {
+  name: 'spinner',
+  text: 'Loading...',
+  delay: 200,
+};
 
 export default Spinner;
