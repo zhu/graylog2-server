216899855327769abb22d835f687934a369c4441
Lukasz Kaminski <lukasz.kaminski@graylog.com>
2022-04-29 13:38:24 +0200

    Beta version of stream-aware field list, based on search engine queries, combining ad-hoc and periodical-storage variant

--- graylog2-server/src/main/java/org/graylog/plugins/views/ViewsBindings.java
+++ graylog2-server/src/main/java/org/graylog/plugins/views/ViewsBindings.java
@@ -185,6 +185,7 @@ public class ViewsBindings extends ViewsModule {
         registerJacksonSubtype(AutoIntervalDTO.class);
 
         bind(SearchJobService.class).to(InMemorySearchJobService.class).in(Scopes.SINGLETON);
+
         bind(MappedFieldTypesService.class).to(MappedFieldTypesServiceImpl.class).in(Scopes.SINGLETON);
         bind(FieldTypeValidation.class).to(FieldTypeValidationImpl.class).in(Scopes.SINGLETON);
 
