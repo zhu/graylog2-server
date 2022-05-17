77f613fa2e759abf9464f5a1cea561e31e8e5005
Dennis Oelkers <dennis@graylog.com>
2022-05-17 16:47:41 +0200

    Some cleanups.

--- graylog-storage-elasticsearch6/src/main/java/org/graylog/storage/elasticsearch6/views/ElasticsearchBackend.java
+++ graylog-storage-elasticsearch6/src/main/java/org/graylog/storage/elasticsearch6/views/ElasticsearchBackend.java
@@ -123,7 +123,7 @@ public class ElasticsearchBackend implements QueryBackend<ESGeneratedQueryContex
 
         // add the optional root query filters
         generateFilterClause(query.filter(), job, query)
-                .map(boolQuery::filter);
+                .ifPresent(boolQuery::filter);
 
         final SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder()
                 .query(boolQuery)
